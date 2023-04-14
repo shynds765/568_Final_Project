fd109afe0e7fd60f853471dbab9988f4a7ef0ea9
dgusis <dgusis10@gmail.com>
2023-04-14 18:37:37 -0400

    Update augEOM.m

--- augEOM.m
+++ augEOM.m
@@ -3,7 +3,7 @@ function Xaugdot = augEOM(t,Xaug, R)
     lambda = Xaug(7:12);
 
     u = u_star_1body(R(1),R(2),R(3),lambda(1),lambda(2),lambda(3),lambda(4),lambda(5),lambda(6),x(1),x(2),x(3),x(4),x(5),x(6));
-    x_dot = x_dot_1body(x(6),nu,u(1),u(2),u(3),x(1),x(2),x(3),x(4),x(5));
+    x_dot = x_dot_1body(x(6),u(1),u(2),u(3),x(1),x(2),x(3),x(4),x(5), x(6));
 
     Xaugdot = [x_dot(1);
                x_dot(2);
