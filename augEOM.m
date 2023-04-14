24fe81b3406efb7fa115726c125ffee952155ea2
dgusis <dgusis10@gmail.com>
2023-04-14 18:32:34 -0400

    Aug Function version 1

--- augEOM.m
+++ augEOM.m
@@ -1,7 +1,20 @@
-function Xaugdot = augEOM(t,Xaug)
-    X = Xaug(1:6);
+function Xaugdot = augEOM(t,Xaug, R)
+    x = Xaug(1:6);
     lambda = Xaug(7:12);
 
-    x_dot_1body(X(6),nu,u1,u2,u3,x1,x2,x3,x4,x5);
+    u = u_star_1body(R(1),R(2),R(3),lambda(1),lambda(2),lambda(3),lambda(4),lambda(5),lambda(6),x(1),x(2),x(3),x(4),x(5),x(6));
+    x_dot = x_dot_1body(x(6),nu,u(1),u(2),u(3),x(1),x(2),x(3),x(4),x(5));
 
+    Xaugdot = [x_dot(1);
+               x_dot(2);
+               x_dot(3);
+               x_dot(4);
+               x_dot(5);
+               x_dot(6);
+               lambda(1);
+               lambda(2);
+               lambda(3);
+               lambda(4);
+               lambda(5);
+               lambda(6)];
 end
