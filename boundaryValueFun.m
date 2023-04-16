05c084a9314489b4daef6339a86eb90c9cb7e907
Dan Qi <qi85@purdue.edu>
2023-04-16 16:43:00 -0400

    code works

--- boundaryValueFun.m
+++ boundaryValueFun.m
@@ -1,16 +1,7 @@
-function Psi = boundaryValueFun(lamda0,Tf,X0,mu,debris_sol,ref_sol)
+function Psi = boundaryValueFun(lamda0,Tf,X0,mu,debris_sol,ref_sol,Q,R,d0,gamma,rho)
     options_ode = odeset('RelTol', 1.0e-12, 'AbsTol', 1.0e-12);
     Xug0 = [X0;lamda0];
 
-    Q = diag(eye(6));
-
-    R = diag(eye(3));
-
-    d0 = 10; % km
-
-    % System Parameters 
-    gamma = 0;
-    rho = 1;
     [~,Xaug] = ode45(@(t,Xaug) augEOM(t,Xaug,Q,R,debris_sol,ref_sol,mu,d0,gamma,rho),[0,Tf], Xug0, options_ode);
     lambda_f = Xaug(end,7:12);
 
