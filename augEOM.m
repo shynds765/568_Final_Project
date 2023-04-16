e9f08140a3f0261bf5d104621e5a4b2d9511c7b7
Dan Qi <qi85@purdue.edu>
2023-04-16 15:00:53 -0400

    fixed mean motion

--- augEOM.m
+++ augEOM.m
@@ -1,4 +1,4 @@
-function Xaugdot = augEOM(t,Xaug,x_target,R,Q,d0,gamma,rho)
+function Xaugdot = augEOM(t,Xaug,x_target,R,Q,d0,gamma,rho,asteroid_hist)
     x = Xaug(1:6);
     lambda = Xaug(7:12);
 
@@ -7,11 +7,14 @@ function Xaugdot = augEOM(t,Xaug,x_target,R,Q,d0,gamma,rho)
         lambda(4),lambda(5),lambda(6),x(1),x(2),x(3),x(4),x(5),x(6));
     
     % Calculate Position of Asteroid (in cartesian)
-    pos_obj = zeros(3,1);
+    X_ast = deval(asteroid_hist,t);
+    pos_obj = X_ast(1:3);
 
+    mu = 3.9860e5;
+    n = sqrt(mu/x(1)^3);
 
     % State and costate dynamics
-    x_dot = x_dot_1body(x(6),u(1),u(2),u(3),x(1),x(2),x(3),x(4),x(5),x(6));
+    x_dot = x_dot_1body(n,u(1),u(2),u(3),x(1),x(2),x(3),x(4),x(5),x(6));
     lambda_dot = lambda_dot_1body(Q(1,1),Q(2,2),Q(3,3),Q(4,4),Q(5,5),...
         Q(6,6),d0,gamma,lambda(1),lambda(2),lambda(3),lambda(4),...
         lambda(5),lambda(6),pos_obj(1),pos_obj(2),pos_obj(3),rho,u(1),u(2),...
--- main.m
+++ main.m
@@ -3,6 +3,17 @@ clear
 options_fsolve = optimset('TolFun',1e-8,'TolX',1e-8,'Display','off');
 options_ode = odeset('RelTol', 1.0e-12, 'AbsTol', 1.0e-12);
 
+
+
 lambda0_guess = zeros(6,1);
 Tf = ;
+
+a0 = 6.7862E3; % km
+e0 = 0;
+i0 = 0;
+RAAN0 = 0;
+w0 = 0;
+M0 = 0;
+
+X0 = [a0;e0;i0;RAAN0;w0;M0];
 lambda0 = fsolve(@(lamda0) boundaryValueFun(lamda0,Tf,X0),lambda0_guess,options_fsolve);
